e27e2402e4fad10af53a50a6adeed99026490aab
Leo <changliyao19@gmail.com>
2023-05-11 16:37:25 -0400

    modified html file and defined variables in script.js

--- script.js
+++ script.js
@@ -25,84 +25,38 @@ const quiz = [
       choices: ["The current object", "The current script", "It has no meaning"],
       answer: "The current object"
     }
-  ];
+];
   
-  // Define variables
-  const startBtn = document.getElementById("start");
-  const timerEl = document.getElementById("timer");
-  const quizEl = document.getElementById("quiz");
-  const questionEl = document.getElementById("question");
-  const choicesEl = document.getElementById("choices");
-  const resultsEl = document.getElementById("results");
-  const scoreEl = document.getElementById("score");
-  const initialsEl = document.getElementById("initials");
-  const submitBtn = document.querySelector("button[type='submit']");
-  let currentQuestion = 0;
-  let correctAnswers = 0;
-  let timeLeft = 60;
-  let timerId;
-  
-  // Add event listener to the start button
-  startBtn.addEventListener("click", startQuiz);
-  
-  // Function to start the quiz
-  function startQuiz() {
-    // Hide the start button
-    startBtn.style.display = "none";
-    // Display the first question
-    displayQuestion();
-    // Start the timer
-    timerId = setInterval(() => {
-      timeLeft--;
-      timerEl.textContent = timeLeft;
-      if (timeLeft === 0) {
-        endQuiz();
-      }
-    }, 1000);
-  }
-  
-  // Function to display a question
-  function displayQuestion() {
-    // Get the current question
-    const question = quiz[currentQuestion];
-    // Display the question
-    questionEl.textContent = question.question;
-    // Display the choices
-    choicesEl.innerHTML = "";
-    question.choices.forEach(choice => {
-      const choiceEl = document.createElement("button");
-      choiceEl.textContent = choice;
-      choiceEl.addEventListener("click", () => {
-        // Check if the answer is correct
-        if (choice === question.answer) {
-          correctAnswers++;
-        }
-        // Display the next question or end the quiz
-        currentQuestion++;
-        if (currentQuestion < quiz.length) {
-          displayQuestion();
-        } else {
-          endQuiz();
-        }
-      });
-      choicesEl.appendChild(choiceEl);
-    });
-  }
-  
-  // Function to end the quiz
-  function endQuiz() {
-    clearInterval(timerId);
-    // Hide the question and choices elements
-    quizEl.style.display = "none";
-    // Display the results element
-    resultsEl.style.display = "block";
-    // Calculate the score
-    const percentage = Math.round(correctAnswers / quiz.length * 100);
-    scoreEl.textContent = `${percentage}%`;
-    // Save the score to local storage
-    submitBtn.addEventListener("click", () => {
-      const scores = JSON.parse(localStorage.getItem("scores")) || [];
-      scores.push({ initials: initialsEl.value, score });
-      localStorage.setItem("scores", JSON.stringify(scores));
-    });
-  }
+// Define variables
+const startBtn = document.getElementById("start");
+const timerEl = document.getElementById("timer");
+const quizEl = document.getElementById("quiz");
+const questionEl = document.getElementById("question");
+const choicesEl = document.getElementById("choices");
+const resultsEl = document.getElementById("results");
+const scoreEl = document.getElementById("score");
+const initialsEl = document.getElementById("initials");
+const submitBtn = document.querySelector("button[type='submit']");
+let currentQuestion = 0;
+let correctAnswers = 0;
+let timeLeft = 60;
+let timerId;
+
+// Add event listener to the start button
+startBtn.addEventListener("click", startQuiz);
+
+// Function to start the quiz
+function startQuiz() {
+  // Hide the start button
+  startBtn.style.display = "none";
+  // Display the first question
+  displayQuestion();
+  // Start the timer
+  timerId = setInterval(() => {
+    timeLeft--;
+    timerEl.textContent = timeLeft;
+    if (timeLeft === 0) {
+      endQuiz();
+    }
+  }, 1000);
+}  
